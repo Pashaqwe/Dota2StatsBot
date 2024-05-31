f7d525c90cd9c37c2266a9c6f7855225522f517c
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 02:46:57 +0300

    test

--- netlify/functions/update.ts
+++ netlify/functions/update.ts
@@ -1,6 +1,21 @@
 import { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event) => {
+  if (event.body) {
+    const bodyRequest = {
+      chat_id: JSON.parse(event.body).message.chat.id,
+      text: "I got your message!",
+    };
+
+    await fetch(
+      `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,
+      {
+        method: "POST",
+        body: JSON.stringify(bodyRequest),
+      }
+    );
+  }
+
   console.log("Received an update from Telegram!", event.body);
   return { statusCode: 200 };
 };
