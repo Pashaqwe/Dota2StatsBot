4a22412e46ecdfa490935eb0f6bfd79d334c63ce
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 20:08:28 +0300

    test

--- api/bot.ts
+++ api/bot.ts
@@ -6,14 +6,14 @@ import express from "express";
 
 const bot = new Bot("7261527510:AAFIYYCShhudAR0m0InYTZwAhLswVS9zyFw");
 
-const app = express();
-app.use(express.json());
-app.use(`/api/bot`, webhookCallback(bot));
-
 bot.command("start", (ctx) => {
   return ctx.reply("asd");
 });
 
 bot.start();
+
+const app = express();
+app.use(express.json());
+app.use(`/api/bot`, webhookCallback(bot));
 
 export default app;
