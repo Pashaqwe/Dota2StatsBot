d359bafe4e9a4194c3c87734bf258c8f6687f441
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 17:43:02 +0300

    asd

--- api/inedx.ts
+++ api/inedx.ts
@@ -1,4 +1,29 @@
 import { webhookCallback } from "grammy";
 import { bot } from "../src/config";
 
-export default webhookCallback(bot);
+type MiddlewareFn = (
+  req: unknown,
+  res: unknown,
+  next: (err?: unknown) => void
+) => void;
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: unknown, res: unknown, fn: MiddlewareFn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+async function handler(req: unknown, res: unknown) {
+  // Run the middleware
+  await runMiddleware(req, res, webhookCallback(bot));
+}
+
+export default handler;
