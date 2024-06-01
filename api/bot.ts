d8f9aa9f6848cefa176d5baa0ea05229bf54b6d3
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 19:17:55 +0300

    ase

--- api/bot.ts
+++ api/bot.ts
@@ -5,10 +5,35 @@ import { Bot, webhookCallback } from "grammy";
 
 const bot = new Bot("7261527510:AAFIYYCShhudAR0m0InYTZwAhLswVS9zyFw");
 
-export default webhookCallback(bot, "std/http");
-
 bot.command("start", (ctx) => {
   return ctx.reply("asd");
 });
 
 bot.start();
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: unknown,
+  res: unknown,
+  fn: (...args: unknown[]) => Promise<unknown>
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+async function handler(req: unknown, res: unknown) {
+  // Run the middleware
+  console.log(req, res);
+
+  await runMiddleware(req, res, webhookCallback(bot));
+}
+
+export default handler;
