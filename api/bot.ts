e2853e626d91ab0d1c49a50d4d4872c1f3fc46ee
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-02 01:55:40 +0300

    test

--- api/bot.ts
+++ api/bot.ts
@@ -3,8 +3,8 @@ import express from "express";
 
 const bot = new Bot("7261527510:AAFIYYCShhudAR0m0InYTZwAhLswVS9zyFw");
 
-bot.command("start", (ctx) => {
-  return ctx.reply("Hello!");
+bot.command("start", async (ctx) => {
+  await ctx.reply("Hello!");
 });
 
 bot.start();
