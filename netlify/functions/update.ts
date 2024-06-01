a4527bfdbc7b2bd33eaf366ff827404aae8e8521
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 03:44:52 +0300

    test

--- netlify/functions/update.ts
+++ netlify/functions/update.ts
@@ -4,6 +4,14 @@ import { bot } from "../../src/config";
 const handler: Handler = async (event) => {
   const update = event.body;
 
+  await fetch("/article/fetch/post/user", {
+    method: "POST",
+    body: JSON.stringify({
+      chat_id: JSON.parse(event.body).message.chat.id,
+      text: "I got your message!",
+    }),
+  });
+
   bot.handleUpdate(update, "message");
 
   console.log("Received an update from Telegram!", event.body);
