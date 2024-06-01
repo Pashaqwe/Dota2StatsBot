d65337c1a6c73b4e78fc66bb2ee9c55ddeff6f40
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 21:46:21 +0300

    asd

--- api/bot.ts
+++ api/bot.ts
@@ -1,22 +1,17 @@
 import { Bot, webhookCallback } from "grammy";
 import express from "express";
 
-// const token = process.env.TOKEN;
-// if (!token) throw new Error(`BOT_TOKEN is unset ${token}`);
-
 const bot = new Bot("7261527510:AAFIYYCShhudAR0m0InYTZwAhLswVS9zyFw");
 
 bot.command("start", (ctx) => {
-  return ctx.reply("asd");
+  return ctx.reply("Hello!");
 });
 
 bot.start();
 
 const app = express();
 app.use(express.json());
-app.use(`/api/bot`, webhookCallback(bot));
-app.use(express.json()); // Добавляем middleware для парсинга JSON теле запроса
+app.use("/api/bot", webhookCallback(bot)); // Убедитесь, что webhookCallback правильно импортирован и используется
+// app.use(express.json()); // Удалено дубликат
 
 export default app;
-// Исправляем определение маршрута
-app.use("/api/bot", webhookCallback(bot, "std/http")); // Ук
