aebe87e8c27f6e22eace1197fd4a66e89b3f9e98
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 19:08:49 +0300

    ase

--- api/bot.ts
+++ api/bot.ts
@@ -5,10 +5,10 @@ import { Bot, webhookCallback } from "grammy";
 
 const bot = new Bot("7261527510:AAFIYYCShhudAR0m0InYTZwAhLswVS9zyFw");
 
+export default webhookCallback(bot, "std/http");
+
 bot.command("start", (ctx) => {
   return ctx.reply("asd");
 });
 
 bot.start();
-
-export default webhookCallback(bot);
