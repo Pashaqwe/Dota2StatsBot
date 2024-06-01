a45f63121f366d9a95e61e73ee43200e9d128552
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 20:19:00 +0300

    asd

--- api/bot.ts
+++ api/bot.ts
@@ -1,16 +1,8 @@
-import { Bot, webhookCallback } from "grammy";
+import { webhookCallback } from "grammy";
 import express from "express";
-
-const bot = new Bot("7261527510:AAFIYYCShhudAR0m0InYTZwAhLswVS9zyFw");
-
-bot.command("start", (ctx) => {
-  return ctx.reply("Hello!");
-});
-
-bot.start();
+import { bot } from "../src/config";
 
 const app = express();
-app.use(express.json()); // Добавляем middleware для парсинга JSON теле запроса
+app.use(express.json());
 
-// Исправляем определение маршрута
-app.use("/api/bot", webhookCallback(bot, "std/http")); // Ук
+app.use("/api/bot", webhookCallback(bot, "std/http"));
