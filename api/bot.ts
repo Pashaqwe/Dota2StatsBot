b1289110fa94e2225784d6f019ad2e482d5b9459
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 21:39:03 +0300

    asd

--- api/bot.ts
+++ api/bot.ts
@@ -1,7 +1,7 @@
 import { Bot, webhookCallback } from "grammy";
 import express from "express";
 
-const bot = new Bot(String(process.env.TOKEN));
+const bot = new Bot("7261527510:AAFIYYCShhudAR0m0InYTZwAhLswVS9zyFw");
 
 const app = express();
 app.use(express.json());
