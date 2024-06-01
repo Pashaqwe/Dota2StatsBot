28904e2c7d61c77c280ac108c4056ffd0d431296
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-01 05:40:30 +0300

    test12

--- api/commands/damir_stats/damir_stats.ts
+++ api/commands/damir_stats/damir_stats.ts
@@ -3,7 +3,6 @@ import { getUserMatchesListMessage } from "../../features";
 
 export const damir_stats = async (ctx: TContext) => {
   if (!process.env.DAMIR_ID) throw new Error("Не найдена переменная DAMIR_ID");
-  console.log("s");
 
   await getUserMatchesListMessage({ ctx, userId: process.env.DAMIR_ID });
 };
