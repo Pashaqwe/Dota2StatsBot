4051570abfda8b013a1786e0e84965cffb54221e
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-03 02:46:22 +0300

    Рефакторинг некоторых файлов, изменение названий

--- src/utils/launch/launch.ts
+++ src/utils/launch/launch.ts
@@ -14,7 +14,7 @@ export const development = async (bot: Bot<TContext>): Promise<void> => {
 export const production = async (bot: Bot<TContext>): Promise<void> => {
   try {
     await bot.api.setWebhook(
-      `${process.env.VERCEL_PROJECT_PRODUCTION_URL}/api/bot`
+      `${process.env.VERCEL_PROJECT_PRODUCTION_URL}/api/index`
     );
     console.log(`[SERVER] Bot starting webhook`);
   } catch (e) {
