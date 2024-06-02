381159904f34ea9463a1b87548e2914637721d04
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-02 16:22:49 +0300

    fix

--- api/bot.ts
+++ api/bot.ts
@@ -5,9 +5,8 @@ import express from "express";
 import bot from "../src/config/config";
 
 const app = express();
-app.use(express.json());
 
-// Используйте webhookCallback для обработки входящих обновлений
+app.use(express.json());
 app.use("/api/bot", webhookCallback(bot));
 
 export default app;
