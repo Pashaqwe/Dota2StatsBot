f4114f261c03a2f931be170dba2a7d85bc1fcd21
Pasha <pavel.votchinnikov@rtkit.corp>
2024-06-03 03:52:32 +0300

    Тестовый коммит для отдалдки вебхука

--- src/utils/launch/launch.ts
+++ src/utils/launch/launch.ts
@@ -2,6 +2,8 @@ import { Bot } from "grammy";
 import { TContext } from "../../config";
 
 export const development = async (bot: Bot<TContext>): Promise<void> => {
+  console.log(process.env.NODE_ENV, "222222222222222");
+
   try {
     await bot.api.deleteWebhook();
     console.log("[SERVER] Bot starting polling");
@@ -12,6 +14,7 @@ export const development = async (bot: Bot<TContext>): Promise<void> => {
 };
 
 export const production = async (bot: Bot<TContext>): Promise<void> => {
+  console.log(process.env.NODE_ENV, "222222222222222");
   try {
     await bot.api.setWebhook(`${process.env.WEBHOOK_DOMAIN}/api/index`);
     console.log(`[SERVER] Bot starting webhook`);
